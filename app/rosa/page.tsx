9b30da8221f3bccc47068a942ca24bf720598be7
DelCaribeX <powerglare893@gmail.com>
2026-06-13 20:38:44 -0500

    Delete app/rosa directory

--- app/rosa/page.tsx
+++
@@ -1,9 +0,0 @@
-import RosaPython from "../../components/RosaPython";
-
-export default function RosaPage() {
-  return (
-    <main className="min-h-screen bg-black text-white p-10">
-      <RosaPython />
-    </main>
-  );
-}
